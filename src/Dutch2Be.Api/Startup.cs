211a7b426a3196609a2fa46bad7b511d3715cd8d
Yerbol Kalykhbergenov <yerbol.kalykh@gmail.com>
2021-03-12 23:31:24 +0100

    Moved files

--- src/Dutch2Be.Api/Startup.cs
+++ src/Dutch2Be.Api/Startup.cs
@@ -1,6 +1,8 @@
+using Dutch2Be.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,8 +10,20 @@ namespace Dutch2Be.Api
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddInfrastructure(Configuration);
+
+
+            services.AddControllers();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -21,12 +35,9 @@ namespace Dutch2Be.Api
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+            app.UseEndpoints(cfg =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                cfg.MapControllers();
             });
         }
     }
