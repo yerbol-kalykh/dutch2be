8cb369ee85db9b55d0b0f25fd9542ff608c93e09
Yerbol Kalykhbergenov <yerbol.kalykh@gmail.com>
2021-03-12 22:52:41 +0100

    Created ApplicationDbContext with Words entity

--- Dutch2Be.Infrastructure/Persistence/ApplicationDbContext.cs
+++ Dutch2Be.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,7 +1,14 @@
-﻿namespace Dutch2Be.Infrastructure.Persistence
-{
-    public class ApplicationDbContext
-    {
+﻿using Dutch2Be.Domain.Data.Entities;
+using Microsoft.EntityFrameworkCore;
 
+namespace Dutch2Be.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Word> Words{ get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
     }
 }
