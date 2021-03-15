af92cd855d0d2921db19c604c577907b14973e52
Yerbol Kalykhbergenov <yerbol.kalykh@gmail.com>
2021-03-15 22:44:15 +0100

    Created GetWordByIdQuery class with validator

---
+++ src/Dutch2Be.Application/Words/Queries/GetWordById/GetWordByIdQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Dutch2Be.Application.Words.Queries.GetWordById
+{
+    public class GetWordByIdQueryValidator : AbstractValidator<GetWordByIdQuery>
+    {
+        public GetWordByIdQueryValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotNull()
+                .NotEmpty().WithMessage("Id is required.");
+        }
+    }
+}
