972ada2298759cee2d209b0c88c1931ad5825c24
Bassam Hajar <hasa374777@exactsoftware.com>
2021-03-15 12:10:59 +0100

    basic header

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,15 @@
+import { CssBaseline } from "@material-ui/core";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
+import ThemeProvider from "./style/ThemeProvider.jsx";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
