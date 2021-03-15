9792eb3fa5fdd2fa4acb5dc7264ba65951f8a5d8
Yerbol Kalykhbergenov <yerbol.kalykh@gmail.com>
2021-03-15 22:42:17 +0100

    Created WordDto class

---
+++ src/Dutch2Be.Application/Words/Models/WordDto.cs
@@ -0,0 +1,11 @@
+﻿using Dutch2Be.Domain.Data.Enums;
+
+namespace Dutch2Be.Application.Words.Models
+{
+    public class WordDto
+    {
+        public string Id { get; set; }
+
+        public Article Article { get; set; }
+    }
+}
