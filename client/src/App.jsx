74156bbd028e22f5964b5cdb42c8eb7338fe6ead
Bassam Hajar <hasa374777@exactsoftware.com>
2021-03-15 11:29:12 +0100

    basic layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Body from "./layout/Body";
+import Footer from "./layout/Footer";
+import Header from "./layout/Header";
 
-const App = () => {
-  return <div>app</div>;
-};
+const App = () => (
+  <>
+    <Header />
+    <Body />
+    <Footer />
+  </>
+);
 
 export default App;
---
+++ client/src/layout/Body.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Body = () => {
+  return <div>body</div>;
+};
+
+export default Body;
