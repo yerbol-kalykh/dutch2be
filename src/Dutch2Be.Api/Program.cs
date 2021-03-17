f9514c491dbc316a7d3a71a68cb0229b0b39334d
Yerbol Kalykhbergenov <yerbol.kalykh@gmail.com>
2021-03-17 21:01:23 +0100

    Created seed data for words.

--- src/Dutch2Be.Api/Program.cs
+++ src/Dutch2Be.Api/Program.cs
@@ -1,13 +1,38 @@
+using Dutch2Be.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
 
 namespace Dutch2Be.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var environment = services.GetRequiredService<IWebHostEnvironment>();
+
+                try
+                {
+                    await ApplicationDbContextSeed.SeedWordsAsync(services, environment.ContentRootPath);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+
+                    logger.LogError(ex, "An error occurred while migrating or seeding the database.");
+                }
+            }
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ src/Dutch2Be.Infrastructure/Persistence/ApplicationDbContextSeed.cs
@@ -0,0 +1,39 @@
+﻿using Dutch2Be.Domain.Data.Entities;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dutch2Be.Infrastructure.Persistence
+{
+    public class ApplicationDbContextSeed
+    {
+        public static async Task SeedWordsAsync(IServiceProvider serviceProvider, string contentRootPath)
+        {
+            var context = serviceProvider.GetRequiredService<ApplicationDbContext>();
+
+            if (!context.Words.Any())
+            {
+                string fullPath = Path.Combine(contentRootPath, @"resources\dutch.json");
+
+                if (File.Exists(fullPath))
+                {
+                    using (var reader = new StreamReader(fullPath))
+                    {
+                        string wordsJsonString = reader.ReadToEnd();
+
+                        var words = JsonConvert.DeserializeObject<IEnumerable<Word>>(wordsJsonString);
+
+                        await context.Words.AddRangeAsync(words);
+                    }
+
+                }
+            }
+            await context.SaveChangesAsync();
+        }
+    }
+}
