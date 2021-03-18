f254209ea5d0020ff4bbb9616731ad518fba3209
Yerbol Kalykhbergenov <yerbol.kalykh@gmail.com>
2021-03-18 21:05:43 +0100

    Added Cors

--- src/Dutch2Be.Api/Startup.cs
+++ src/Dutch2Be.Api/Startup.cs
@@ -3,16 +3,20 @@ using Dutch2Be.Application;
 using Dutch2Be.Infrastructure;
 using Dutch2Be.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace Dutch2Be.Api
 {
     public class Startup
     {
+        private readonly string AllowSpecificOrigins = "AllowSpecificOrigins";
+
         public IConfiguration Configuration { get; }
 
         public Startup(IConfiguration configuration)
@@ -31,7 +35,16 @@ namespace Dutch2Be.Api
 
             services.AddHealthChecks().AddDbContextCheck<ApplicationDbContext>();
 
+            services.AddCors(ConfigureCors);
+
             services.AddControllers(ConfigureMvcOptions);
+        }
+
+        private void ConfigureCors(CorsOptions options)
+        {
+            options.AddPolicy(name: AllowSpecificOrigins,   
+                    builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+            );           
         }
 
         private void ConfigureMvcOptions(MvcOptions options)
@@ -59,6 +72,8 @@ namespace Dutch2Be.Api
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(AllowSpecificOrigins);
 
             app.UseEndpoints(cfg =>
             {
