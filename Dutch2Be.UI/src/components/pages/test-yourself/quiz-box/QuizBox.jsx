f1102c12fdede83efd108b76703564f069155ae0
Bassam Hajar <hasa374777@exactsoftware.com>
2021-03-25 18:32:05 +0100

    update quiz page style - buttons

--- Dutch2Be.UI/src/components/pages/test-yourself/quiz-box/QuizBox.jsx
+++ Dutch2Be.UI/src/components/pages/test-yourself/quiz-box/QuizBox.jsx
@@ -1,40 +1,50 @@
 import { Button, Container, Grid, Paper } from "@material-ui/core";
 import React from "react";
 import { useStyles } from "./QuizBoxStyle";
+import clsx from "clsx";
 
 const QuizBox = () => {
   const {
     boxContainer,
-    answer,
+    display,
     articleBtn,
     answerBtnsGrid,
-    wordGrid,
+    displayGrid,
+    de,
+    het,
+    noArticle,
+    wrongArticle,
   } = useStyles();
 
   return (
     <Grid container className={boxContainer}>
-      <Grid item container xs={12} className={wordGrid}>
-        <Paper className={answer}>word</Paper>
+      <Grid item container xs={12} className={displayGrid}>
+        <Paper elevation={0} className={display}>
+          word
+        </Paper>
       </Grid>
 
-      <Grid item container xs={12}>
-        <Grid item xs={3} className={answerBtnsGrid}>
-          <Button variant="contained" className={articleBtn}>
+      <Grid item container xs={12} className={answerBtnsGrid}>
+        <Grid item xs={2}>
+          <Button variant="contained" className={clsx(articleBtn, de)}>
             De
           </Button>
         </Grid>
-        <Grid item xs={3} className={answerBtnsGrid}>
-          <Button variant="contained" className={articleBtn}>
+        <Grid item xs={2}>
+          <Button variant="contained" className={clsx(articleBtn, het)}>
             Het
           </Button>
         </Grid>
-        <Grid item xs={3} className={answerBtnsGrid}>
-          <Button variant="contained" className={articleBtn}>
+        <Grid item xs={2}>
+          <Button variant="contained" className={clsx(articleBtn, noArticle)}>
             None
           </Button>
         </Grid>
-        <Grid item xs={3} className={answerBtnsGrid}>
-          <Button variant="contained" className={articleBtn}>
+        <Grid item xs={2}>
+          <Button
+            variant="contained"
+            className={clsx(articleBtn, wrongArticle)}
+          >
             Not a Dutch word!
           </Button>
         </Grid>
--- Dutch2Be.UI/src/components/pages/test-yourself/quiz-box/QuizBoxStyle.js
+++ Dutch2Be.UI/src/components/pages/test-yourself/quiz-box/QuizBoxStyle.js
@@ -5,16 +5,17 @@ export const useStyles = makeStyles((theme) =>
     boxContainer: {
       minHeight: "30vh",
       minWidth: "400px",
-      border: "1px dashed #333",
       borderRadius: "4px",
+      background: "#fff",
+      // border: "1px dashed #333",
     },
 
-    wordGrid: {
+    displayGrid: {
       height: "20vh",
       // border: "1px dashed #333",
     },
 
-    answer: {
+    display: {
       // border: "1px dashed #333",
       display: "flex",
       justifyContent: "center",
@@ -23,25 +24,47 @@ export const useStyles = makeStyles((theme) =>
       width: "100%",
     },
 
-    articleBtn: {
-      width: "100%",
-      height: "100%",
-      transition: "background .5s, color .5s",
-      transform: "toLowerCase",
-      textTransform: "none",
-      "&:hover": {
-        background: "green",
-        fontSize: "1rem",
-        color: "#fff",
-      },
-    },
-
     answerBtnsGrid: {
       height: "10vh",
       display: "flex",
       justifyContent: "center",
       alignItems: "center",
-      border: "1px dashed #000",
+      // border: "1px dashed #000",
+    },
+
+    articleBtn: {
+      minWidth: "90%",
+      minHeight: "8vh",
+      transition: "background .5s, color .5s",
+      textTransform: "none",
+      borderRadius: "50%",
+      "&:hover": {
+        color: "#fff",
+      },
+    },
+
+    de: {
+      "&:hover": {
+        background: "green",
+      },
+    },
+
+    het: {
+      "&:hover": {
+        background: "purple",
+      },
+    },
+
+    noArticle: {
+      "&:hover": {
+        background: "#777",
+      },
+    },
+
+    wrongArticle: {
+      "&:hover": {
+        background: "orange",
+      },
     },
   })
 );
