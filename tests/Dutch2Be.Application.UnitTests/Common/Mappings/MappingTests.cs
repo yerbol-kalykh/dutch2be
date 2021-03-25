844ffdc5dd3ce0519f42612adcc4274aef958442
Yerbol Kalykhbergenov <yerbol.kalykh@gmail.com>
2021-03-25 23:19:30 +0100

    AZ#21 Formatted the code.

--- tests/Dutch2Be.Application.UnitTests/Common/Mappings/MappingTests.cs
+++ tests/Dutch2Be.Application.UnitTests/Common/Mappings/MappingTests.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Dutch2Be.Application.UnitTests.Common.Mappings
+﻿namespace Dutch2Be.Application.UnitTests.Common.Mappings
 {
     public class MappingTests
     {
