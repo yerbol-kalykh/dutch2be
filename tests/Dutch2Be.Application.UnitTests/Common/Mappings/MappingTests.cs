e8bc3df05b5fca81fc4307daada6a6f037e38d4d
Yerbol Kalykhbergenov <yerbol.kalykh@gmail.com>
2021-03-25 23:17:25 +0100

    #21 Created emtpy nunit test solution for Application

---
+++ tests/Dutch2Be.Application.UnitTests/Common/Mappings/MappingTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dutch2Be.Application.UnitTests.Common.Mappings
+{
+    public class MappingTests
+    {
+
+    }
+}
