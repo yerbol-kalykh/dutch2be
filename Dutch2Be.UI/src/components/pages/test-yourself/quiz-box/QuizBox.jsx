44d279da186b3736f567ea105aff7bc936bb88db
Bassam Hajar <hasa374777@exactsoftware.com>
2021-03-25 18:39:21 +0100

    basic fetch for quizArr

--- Dutch2Be.UI/src/components/pages/test-yourself/quiz-box/QuizBox.jsx
+++ Dutch2Be.UI/src/components/pages/test-yourself/quiz-box/QuizBox.jsx
@@ -2,6 +2,7 @@ import { Button, Container, Grid, Paper } from "@material-ui/core";
 import React from "react";
 import { useStyles } from "./QuizBoxStyle";
 import clsx from "clsx";
+import { useHttpClient } from "../../../../hooks/http-hook";
 
 const QuizBox = () => {
   const {
@@ -16,6 +17,20 @@ const QuizBox = () => {
     wrongArticle,
   } = useStyles();
 
+  const { sendRequest } = useHttpClient();
+
+  const clickAnswerBtn = async () => {
+    try {
+      const fetchQuizArr = await sendRequest(`/api/words`, "GET", null, {
+        "Content-Type": "json/application",
+      });
+
+      console.log(fetchQuizArr);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Grid container className={boxContainer}>
       <Grid item container xs={12} className={displayGrid}>
@@ -26,17 +41,29 @@ const QuizBox = () => {
 
       <Grid item container xs={12} className={answerBtnsGrid}>
         <Grid item xs={2}>
-          <Button variant="contained" className={clsx(articleBtn, de)}>
+          <Button
+            variant="contained"
+            className={clsx(articleBtn, de)}
+            onClick={clickAnswerBtn}
+          >
             De
           </Button>
         </Grid>
         <Grid item xs={2}>
-          <Button variant="contained" className={clsx(articleBtn, het)}>
+          <Button
+            variant="contained"
+            className={clsx(articleBtn, het)}
+            onClick={clickAnswerBtn}
+          >
             Het
           </Button>
         </Grid>
         <Grid item xs={2}>
-          <Button variant="contained" className={clsx(articleBtn, noArticle)}>
+          <Button
+            variant="contained"
+            className={clsx(articleBtn, noArticle)}
+            onClick={clickAnswerBtn}
+          >
             None
           </Button>
         </Grid>
@@ -44,6 +71,7 @@ const QuizBox = () => {
           <Button
             variant="contained"
             className={clsx(articleBtn, wrongArticle)}
+            onClick={clickAnswerBtn}
           >
             Not a Dutch word!
           </Button>
