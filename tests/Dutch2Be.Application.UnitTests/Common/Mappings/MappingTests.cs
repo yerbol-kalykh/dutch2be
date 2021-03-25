d790e91e51ea7001267db117770f723eb79da4be
Yerbol Kalykhbergenov <yerbol.kalykh@gmail.com>
2021-03-25 23:25:14 +0100

    AZ #20 Created test for checking automapper configuration

--- tests/Dutch2Be.Application.UnitTests/Common/Mappings/MappingTests.cs
+++ tests/Dutch2Be.Application.UnitTests/Common/Mappings/MappingTests.cs
@@ -1,7 +1,28 @@
-﻿namespace Dutch2Be.Application.UnitTests.Common.Mappings
+﻿using AutoMapper;
+using Dutch2Be.Application.Common.Mappings;
+using NUnit.Framework;
+
+namespace Dutch2Be.Application.UnitTests.Common.Mappings
 {
     public class MappingTests
     {
+        private readonly IConfigurationProvider _configuration;
+        private readonly IMapper _mapper;
 
+        public MappingTests()
+        {
+            _configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            _mapper = _configuration.CreateMapper();
+        }
+
+        [Test]
+        public void ShouldHaveValidConfiguration()
+        {
+            _configuration.AssertConfigurationIsValid();
+        }
     }
 }
